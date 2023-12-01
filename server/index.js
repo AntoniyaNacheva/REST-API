022eb19dadd73bffa1c06d5e6655cca4cac9fcd5
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-12-01 14:35:24 +0200

    MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const cors = require('./middlewares/cors');
+const mongoose = require('mongoose');
 
-const app = express();
+const connectionString = 'mongodb://127.0.0.1:27017/furniture';
 
-app.use(express.json());
-app.use(cors());
+start();
+
+async function start() {
+
+	await mongoose.connect(connectionString);
+	console.log('Database connected!');
+
+	const app = express();
+
+	app.use(express.json());
+	app.use(cors());
+
+	app.get('/', (req, res) => {
+		res.json({ message: 'REST Service' });
+	});
+
+	app.listen(3030, () => console.log('REST service started!'));
+}
