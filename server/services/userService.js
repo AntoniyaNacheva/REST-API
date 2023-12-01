a35274c7f5ab6aa4fa19f02cf1bb7525119bde6b
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-12-01 14:34:43 +0200

    User Service

---
+++ server/services/userService.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const secret = 'akjd84655jdskoa';
+
+async function register(email, password) {
+	const existingUser = await User.findOne({ email })
+		.collation({ locale: 'en', strength: 2 });
+
+	if (existingUser) {
+		throw new Error('Email is taken!');
+	}
+
+	const user = await User.create({
+		email,
+		hashedPassword: bcrypt.hash(password, 10)
+	});
+
+	return createToken(user);
+}
+
+async function login(email, password) {
+
+}
+
+async function logout() {
+
+}
+
+function createToken(user) {
+	const payload = {
+		_id: user._id,
+		email: user.email
+	};
+
+	return jwt.sign(payload, secret);
+} 
