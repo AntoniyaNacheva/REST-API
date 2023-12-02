7de034c36478df2ecf1ba8b5a4126d44e9361cf1
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-12-02 19:00:59 +0200

    Item Service

---
+++ server/services/itemService.js
@@ -0,0 +1,39 @@
+const Item = require('../models/Item');
+
+async function getAll() {
+	return Item.find({});
+}
+
+async function getById(id) {
+	return Item.findById(id);
+}
+
+async function create(item) {
+	return Item.create(item);
+}
+
+async function update(id, item) {
+	const existingItem = await Item.findById(id);
+
+	existingItem.make = item.make;
+	existingItem.model = item.model;
+	existingItem.year = item.year;
+	existingItem.description = item.description;
+	existingItem.price = item.price;
+	existingItem.img = item.img;
+	existingItem.material = item.material;
+
+	return existingItem.save();
+}
+
+async function deleteById(id) {
+	return Item.findByIdAndDelete(id);
+}
+
+module.exports = {
+	getAll,
+	getById,
+	create,
+	update,
+	deleteById
+}
